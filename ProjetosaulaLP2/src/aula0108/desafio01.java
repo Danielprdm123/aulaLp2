aac8c7a4dcf0c94b302fce90045118d94f0a6c3b
É isso ai gordox < isso ai gordox@computador>
2019-08-02 12:51:46 -0300

    teste

--- ProjetosaulaLP2/src/aula0108/desafio01.java
+++ ProjetosaulaLP2/src/aula0108/desafio01.java
@@ -3,6 +3,9 @@ package aula0108;
 public class desafio01 {
 	public static void main(String[] args) {
 		int [][] matriz = new int [5][5];
+		for(int i = 0;i< matriz.length;i++) {
+			
+		}
 	}
-
+ 
 }
