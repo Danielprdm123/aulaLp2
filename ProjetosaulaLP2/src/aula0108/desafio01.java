912bc0fc898f5cad290aca0005e652ad33517b06
É isso ai gordox < isso ai gordox@computador>
2019-08-02 16:04:49 -0300

    desafio01

--- ProjetosaulaLP2/src/aula0108/desafio01.java
+++ ProjetosaulaLP2/src/aula0108/desafio01.java
@@ -3,9 +3,33 @@ package aula0108;
 public class desafio01 {
 	public static void main(String[] args) {
 		int [][] matriz = new int [5][5];
-		for(int i = 0;i< matriz.length;i++) {
-			
+		long inicio = System.currentTimeMillis();
+		System.out.println("Tempo inicial = " + inicio);
+		for (int i=0; i< matriz.length;i++) {
+			for(int j = 0;j<matriz.length;j++) {
+				int pegaval = (int) ((int) 1 + (Math.random() * 26));
+				int per =0;
+				while(per < matriz.length) {
+					if(matriz[i][per] == pegaval) {
+						 pegaval = (int) ((int) 1 + (Math.random() * 26));
+						 per =0;
+					}
+					per++;
+				}
+				matriz[i][j] = pegaval;
+			}
 		}
+	 	
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j <matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+ long tempfinal = System.currentTimeMillis();
+	System.out.println("Tempo final = " + inicio);
+	System.out.println(tempfinal - inicio);
+	
 	}
- 
 }
