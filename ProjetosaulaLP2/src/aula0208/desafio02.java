65aa7ffe549e233c00e479a40841ed3432d772db
É isso ai gordox < isso ai gordox@computador>
2019-08-06 16:33:21 -0300

    teste

--- ProjetosaulaLP2/src/aula0208/desafio02.java
+++ ProjetosaulaLP2/src/aula0208/desafio02.java
@@ -4,22 +4,5 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class desafio02 {
-	public static void Print(int matriz[][]) {
-		Instant inicio = Instant.now();
-		
-		try {
-			Thread.sleep(1000); // fazer processador esperar
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Instant fim = Instant.now();
-		Duration duracao = Duration.between(inicio, fim);
-		System.out.println("Duração em nanos segundos" + duracao.toNanos());
-		System.out.println("Duração em minutos" + duracao.toMinutes());
-		System.out.println("Duração em horas" + duracao.toHours());
-		System.out.println("Duração em milissegundos" + duracao.toMillis());
-		System.out.println("Duração em dias" + duracao.toDays());
-	}
+
 }
